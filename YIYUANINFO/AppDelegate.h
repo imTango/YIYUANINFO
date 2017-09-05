dd0f78e7bbda82cbea70362908aafecc81a2e4bf
❤️❤️嘉欣❤️ <104952129@qq.com>
2017-09-05 20:36:35 +0800

    Initial Commit

---
+++ YIYUANINFO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YIYUANINFO
+//
+//  Created by VINCENT on 2017/9/5.
+//  Copyright © 2017年 北京京壹元资讯信息服务有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
